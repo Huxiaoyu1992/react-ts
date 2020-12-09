575c0c5a3cc5b3470fbc7ef827e0bda334705ff7
huxiaoyu <1163567625@qq.com>
2020-12-09 22:33:52 +0800

    state-hook

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,8 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment } from 'react'
 
+// const Button: React.FC = () => {
+//   const [state, setstate] = useState({num: 0, on: true})
+//   return (
+//     <Fragment>
+//       <button onClick={() => setstate({ num: state.num + 1, on: !state.on })}>{state.num}</button>
+//       <button onClick={() => setstate({ num: state.num + 1, on: !state.on })}>{state.on ? '开': '关'}</button>
+//     </Fragment>
+//   )
+// }
+
+// 上面的写法可以换成下面的写法---useState使在函数组件中使用私有的state成为可能
 const Button: React.FC = () => {
-  const [state, setstate] = useState(1)
-return <button onClick={() => setstate(state + 1)}>{state}</button>
+  const [num, setNum] = useState(0)
+  const [status, setStatus] = useState(true)
+  return (
+    <Fragment>
+      <button onClick={() => setNum(num + 1)}>{num}</button>
+      <button onClick={() => setStatus( !status )}>{status ? '开': '关'}</button>
+    </Fragment>
+  )
 }
 
 export default Button
