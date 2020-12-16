d3d1f1e7a6ec88d9ffde274a7938f95d7cc4974f
huxiaoyu <1163567625@qq.com>
2020-12-16 17:16:06 +0800

    useContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext } from 'react';
 import './App.css';
 import Hello from './components/Hello'
 import Button from './components/button'
@@ -16,6 +16,23 @@ interface ISShowResult {
   status: string;
 }
 
+interface IThemeProps {
+  [key: string]: { color: string; background: string }
+}
+
+const theme: IThemeProps = {
+  light: {
+    color: '#000',
+    background: '#efefef'
+  },
+  dark: {
+    color: '#fff',
+    background: '#222'
+  }
+}
+
+export const ThemeContext = createContext(theme.light)
+
 function App() {
   const [show, setShow] = useState(true)
   const position = useMousePosition()
@@ -29,26 +46,28 @@ function App() {
   }
   return (
     <div className="App">
-      <p><button onClick={()=>setShow(!show)}>刷新图片</button></p>
-      {/* 接口 */}
-      <Hello />
-      {/* useState的Hook */}
-      <Button />
-      {show && <MouseTracker />}
-      {/* 自定义hook */}
-      <p>x: {position.x} - y: {position.y}</p>
-      {/* 自定义hook : useUrlLoader */}
-      {loading ? <p>狗狗图片加载中...</p> : <img src={result.message} alt=""/>}
-      {/* class中使用createRef */}
-      <MyRef />
-      {/* 测试：button */}
-      <TestButton />
-      {/* 用useRef实现的上面一个testButton不能实现的功能 */}
-      <UseRefButton/>
-      {/* 测试useRef */}
-      <FuncRefUse/>
-      {/* forwardRef: 用在组件嵌套子组件 需要穿透组件去获取子组件的ref的时候用 */}
-      <Forward/>
+      <ThemeContext.Provider value={theme.light}>
+        <p><button onClick={()=>setShow(!show)}>刷新图片</button></p>
+        {/* 接口 */}
+        <Hello />
+        {/* useState的Hook */}
+        <Button />
+        {show && <MouseTracker />}
+        {/* 自定义hook */}
+        <p>x: {position.x} - y: {position.y}</p>
+        {/* 自定义hook : useUrlLoader */}
+        {loading ? <p>狗狗图片加载中...</p> : <img src={result.message} alt=""/>}
+        {/* class中使用createRef */}
+        <MyRef />
+        {/* 测试：button */}
+        <TestButton />
+        {/* 用useRef实现的上面一个testButton不能实现的功能 */}
+        <UseRefButton/>
+        {/* 测试useRef */}
+        <FuncRefUse/>
+        {/* forwardRef: 用在组件嵌套子组件 需要穿透组件去获取子组件的ref的时候用 */}
+        <Forward/>
+      </ThemeContext.Provider>
     </div>
   );
 }
--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
-
+import React, { useContext } from 'react'
+import { ThemeContext } from '../App'
 interface HelloProps {
   message?: string
 }
 // FunctionComponent是react内置的一个interface
 // FC是FunctionComponent的类型别名
 const Hello: React.FC<HelloProps> = (props) => {
-return <h1>{props.message}</h1>
+  const theme = useContext(ThemeContext)
+  const style = {
+    color: theme.color,
+    background: theme.background
+  }
+  return <h1 style={style}>{props.message}</h1>
 }
 
 Hello.defaultProps = {
